652f924ebd916e1a01bb0fb2bdeb42e2ac99c7c4
Milinda Saranga <milindasaranga@Milindas-MacBook-Pro.local>
2020-06-27 20:15:34 +0400

    javascript hellow world with arrow functions

---
+++ index.js
@@ -0,0 +1,18 @@
+const printHWDefault = () =>{
+    console.log('Hello World');
+  }
+  
+  const printHWValues = (name) =>{
+    console.log('Hello ' + name);
+  }
+  
+  const multiply = (number) => number * 2;
+  
+  const add = (number1, number2) => {
+    return number1 + number2
+  }
+  
+  printHWDefault();
+  printHWValues("World 2");
+  console.log(multiply(3));
+  console.log(add(2,3));
